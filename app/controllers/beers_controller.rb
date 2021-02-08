d9a570e19281f24f90016a928e4e05c5f67f08b9
chrislechner300 <denver929@gmail.com>
2021-02-08 15:38:53 -0700

    added create function

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -9,9 +9,27 @@ class BeersController < ApplicationController
   def show
   end
 
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    @beer = Beer.new(beer_params)
+    if @beer.save
+      # something if valid
+      redirect_to beers_path
+    else
+      #something if not valid
+      render :new
+    end
+  end
+
   private
     def find_beer
       @beer = Beer.find_by_id(params[:id])
     end
 
+    def beer_params
+      params.require(:beer).permit(:name, :style, :brewery, :sample_date)
+    end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,4 @@
 class Beer < ApplicationRecord
+
+  validates :name, presence: true
 end
